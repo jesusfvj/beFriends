3baa57681391fdeda22b071a68521fce575aca96
Daniel Wilson <danielvillenawilson@gmail.com>
2023-02-01 18:50:52 +0100

    change(index): add require

--- index.php
+++ index.php
@@ -1,10 +1,22 @@
+<?php
+require_once("./config/db.php");
+require_once("./Database.php");
+require_once("./DbConection.php");
+require_once("./model/UserModel.php");
+
+$user = new UserModel();
+$user->get();
+echo "<pre>";
+print_r(($user->get()));
+echo "</pre>";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Be_Friends</title>
 </head>
 <body>
     

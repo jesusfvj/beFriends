0dcf3d22b0236cf25dde1a30371235d7c4d4d24d
Miquel <abellagarciamiquel@gmail.com>
2023-02-01 18:16:22 +0100

    feat(feed) html and css feed skeleton

--- feed.php
+++ feed.php
@@ -6,10 +6,108 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>beFriends - feed</title>
+    <link rel="stylesheet" href="./css/global.css">
 </head>
 
-<body>
-
+<body class="feed__body">
+    <aside class="feed__aside">
+        <img src="./assets/logoBeFriends.png" alt="logo" class="feed__aside-logo" />
+        <nav class="feed__nav">
+            <div class="feed__nav-item">
+                <img src="./assets/profile.png" alt="nav-icon" class="nav__image" />
+                <p>Profile</p>
+            </div>
+            <div class="feed__nav-item">
+                <img src="./assets/profile.png" alt="nav-icon" class="nav__image" />
+                <p>Search</p>
+            </div>
+            <div class="feed__nav-item">
+                <img src="./assets/profile.png" alt="nav-icon" class="nav__image" />
+                <p>Logout</p>
+            </div>
+        </nav>
+    </aside>
+    <main class="feed__main">
+        <article class="feed__post">
+            <div class="feed__article-header">
+                <img class="feed__post-profile-img" src="./assets/profileImg.JPG" alt="" />
+                <div>
+                    <p class="feed__post-profile-name">MIQUEL_ABELLA - <span class="feed__post-profile-follow-text">follow</span></p>
+                    <p class="feed__post-timestamp">1 week ago</p>
+                </div>
+            </div>
+            <img class="feed__post-img" src="./assets/mockImage.JPG" alt="" />
+            <div class="feed__article-comments-container">
+                <div class="feed__post-icons-container">
+                    <img class="feed__post-icon" src="./assets/heart.png" alt="" />
+                    <p>5 likes</p>
+                    <img class="feed__post-icon" src="./assets/message.png" alt="" />
+                </div>
+                <div class="feed__post-comments-container">
+                    <div class="feed__post-comment">
+                        <p class="feed__post-comment-author">Wilson</p>
+                        <p class="feed__post-comment-message">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque cum consequuntur iusto illo illum quis sunt facere, ullam expedita, repellendus, ab esse! Velit, saepe quis. Perferendis doloremque ipsum rem beatae.</p>
+                    </div>
+                    <div class="feed__post-comment">
+                        <p class="feed__post-comment-author">Wilson</p>
+                        <p class="feed__post-comment-message">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque cum consequuntur iusto illo illum quis sunt facere, ullam expedita, repellendus, ab esse! Velit, saepe quis. Perferendis doloremque ipsum rem beatae.</p>
+                    </div>
+                    <div class="feed__post-comment">
+                        <p class="feed__post-comment-author">Wilson</p>
+                        <p class="feed__post-comment-message">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque cum consequuntur iusto illo illum quis sunt facere, ullam expedita, repellendus, ab esse! Velit, saepe quis. Perferendis doloremque ipsum rem beatae.</p>
+                    </div>
+                </div>
+            </div>
+        </article>
+        <article class="feed__post">
+            <div class="feed__article-header">
+                <img class="feed__post-profile-img" src="./assets/profileImg.JPG" alt="" />
+                <div>
+                    <p class="feed__post-profile-name">MIQUEL_ABELLA - <span class="feed__post-profile-follow-text">follow</span></p>
+                    <p class="feed__post-timestamp">1 week ago</p>
+                </div>
+            </div>
+            <img class="feed__post-img" src="./assets/mockImage.JPG" alt="" />
+            <div class="feed__article-comments-container">
+                <div class="feed__post-icons-container">
+                    <img class="feed__post-icon" src="./assets/heart.png" alt="" />
+                    <p>5 likes</p>
+                    <img class="feed__post-icon" src="./assets/message.png" alt="" />
+                </div>
+                <div class="feed__post-comments-container">
+                    <div class="feed__post-comment">
+                        <p class="feed__post-comment-author">Wilson</p>
+                        <p class="feed__post-comment-message">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque cum consequuntur iusto illo illum quis sunt facere, ullam expedita, repellendus, ab esse! Velit, saepe quis. Perferendis doloremque ipsum rem beatae.</p>
+                    </div>
+                    <div class="feed__post-comment">
+                        <p class="feed__post-comment-author">Wilson</p>
+                        <p class="feed__post-comment-message">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque cum consequuntur iusto illo illum quis sunt facere, ullam expedita, repellendus, ab esse! Velit, saepe quis. Perferendis doloremque ipsum rem beatae.</p>
+                    </div>
+                    <div class="feed__post-comment">
+                        <p class="feed__post-comment-author">Wilson</p>
+                        <p class="feed__post-comment-message">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque cum consequuntur iusto illo illum quis sunt facere, ullam expedita, repellendus, ab esse! Velit, saepe quis. Perferendis doloremque ipsum rem beatae.</p>
+                    </div>
+                </div>
+            </div>
+        </article>
+    </main>
+    <aside class="feed__friends-suggestions">
+        <div class="feed__friends-suggestions-friends">
+            <p>Suggested friends for you</p>
+            <div class="feed__friends-suggestions-profile">
+                <img class="feed__post-profile-img" src="./assets/profileImg.JPG" alt="" />
+                <p>Jezú Vzqz</p>
+            </div>
+            <div class="feed__friends-suggestions-profile">
+                <img class="feed__post-profile-img" src="./assets/profileImg.JPG" alt="" />
+                <p>DeividPizzi</p>
+            </div>
+            <div class="feed__friends-suggestions-profile">
+                <img class="feed__post-profile-img" src="./assets/profileImg.JPG" alt="" />
+                <p>AfricanitoEater</p>
+            </div>
+        </div>
+    </aside>
 </body>
 
 </html>

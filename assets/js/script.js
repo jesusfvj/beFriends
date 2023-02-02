8f656af861c98a21ea2d8861cae3a1e109748f67
Miquel <abellagarciamiquel@gmail.com>
2023-02-02 13:37:19 +0100

    feat(user):register user method

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,32 +4,35 @@ const friendsSuggestionsContainer = document.querySelector(
 
 const showParagraph = document.querySelectorAll(".paragraph-show__text");
 
-showParagraph.forEach(element => {
-    element.addEventListener("click", togglePassword);
+showParagraph.forEach((element) => {
+  element.addEventListener("click", togglePassword);
 });
-
 
 document.body.addEventListener("load", getUsers());
 function getUsers() {
+  let msg = new SpeechSynthesisUtterance();
+  msg.text = "Hola Wilson, Jesus y sus aliados te saludan";
+  msg.lang = "es-ES";
+  window.speechSynthesis.speak(msg);
   fetch("./controllers/users/getAll.php")
     .then((res) => res.json())
     .then((data) => {
       if (data.length) {
         data.forEach((user) => {
           friendsSuggestionsContainer.innerHTML += `
-            <div class="feed__friends-suggestions-profile">
-                <button onclick="addFriend(event)" class="feed__friends-suggestions-add-btn" userId=${user[0]}>+</button>
-                <img class="feed__post-profile-img" src="./assets/profileImg.JPG" alt="" />
-                <p>${user.name}</p>
-            </div>
-    `;
+              <div class="feed__friends-suggestions-profile">
+                  <button onclick="addFriend(event)" class="feed__friends-suggestions-add-btn" userId=${user[0]}>+</button>
+                  <img class="feed__post-profile-img" src="./assets/profileImg.JPG" alt="" />
+                  <p>${user.name}</p>
+              </div>
+      `;
         });
       } else {
         friendsSuggestionsContainer.innerHTML += `
-            <div class="feed__friends-suggestions-profile">
-               <p>No friends to suggest</p>
-            </div>
-    `;
+              <div class="feed__friends-suggestions-profile">
+                 <p>No friends to suggest</p>
+              </div>
+      `;
       }
     });
 }
@@ -38,10 +41,16 @@ function addFriend(event) {
   console.log("Adding " + event.target.getAttribute("userId"));
 }
 
-
-function togglePassword(event){
-    const clickedShow = document.querySelector("#"+event.target.id);
-    const clickedInput = document.querySelector("#"+event.target.nextElementSibling.id);
-    clickedInput.type=='password'?(clickedInput.type='text', clickedShow.textContent="Hide", clickedShow.style.color="rgb(161, 161, 161)"):
-        (clickedInput.type='password', clickedShow.textContent="Show", clickedShow.style.color="black");
+function togglePassword(event) {
+  const clickedShow = document.querySelector("#" + event.target.id);
+  const clickedInput = document.querySelector(
+    "#" + event.target.nextElementSibling.id
+  );
+  clickedInput.type == "password"
+    ? ((clickedInput.type = "text"),
+      (clickedShow.textContent = "Hide"),
+      (clickedShow.style.color = "rgb(161, 161, 161)"))
+    : ((clickedInput.type = "password"),
+      (clickedShow.textContent = "Show"),
+      (clickedShow.style.color = "black"));
 }

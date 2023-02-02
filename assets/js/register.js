a94c3849867bbc6d79ed9c0eea249268ab14f1a6
Daniel Wilson <danielvillenawilson@gmail.com>
2023-02-02 23:39:47 +0100

    change(display) add display modal and display password verification alert

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,4 +1,7 @@
 const showParagraph = document.querySelectorAll(".paragraph-show__text");
+const passwordAlert = document.querySelector(".password-verification__div--display");
+const sucessAlert = document.querySelector(".modal-success-register__div");
+const errorAlert = document.querySelector(".modal-error-register__div");
 
 showParagraph.forEach((element) => {
   element.addEventListener("click", togglePassword);
@@ -9,19 +12,50 @@ registerForm.addEventListener("submit", sendRegisterData);
 
 function sendRegisterData(event) {
   event.preventDefault();
-  const formData = new FormData();
-  formData.append("fullname", inputNameRegister.value);
-  formData.append("username", inputUserRegister.value);
-  formData.append("email", inputEmailRegister.value);
-  formData.append("password", inputPasswordRegister.value);
-  formData.append("gender", inputGenderRegister.value);
+  if (inputPasswordRepeat.value === inputPasswordRegister.value) {
+    const formData = new FormData();
+    formData.append("fullname", inputNameRegister.value);
+    formData.append("username", inputUserRegister.value);
+    formData.append("email", inputEmailRegister.value);
+    formData.append("password", inputPasswordRegister.value);
+    formData.append("gender", inputGenderRegister.value);
+    fetch("./controllers/users/register.php", {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data[0] === true) {
+          sucessAlert.style.display = "block";
 
-  fetch("./controllers/users/register.php", {
-    method: "POST",
-    body: formData,
+          setTimeout(() => {
+            sucessAlert.style.display = "none";
+          }, 3000);
+          setTimeout(() => {
+            window.location.href = "./index.php";
+          }, 3500);
+          
+        } else{
+          errorAlert.style.display = "block";
+
+          setTimeout(() => {
+            errorAlert.style.display = "none";
+          }, 3000);
+        }
+      });
+
+
+  } else {
+    showPasswordVerification();
+  }
+
+}
+
+function showPasswordVerification() {
+  passwordAlert.style.display = "block";
+  window.addEventListener("click", () => {
+    passwordAlert.style.display = "none";
   })
-    .then((res) => res.json())
-    .then((data) => {});
 }
 
 function togglePassword(event) {

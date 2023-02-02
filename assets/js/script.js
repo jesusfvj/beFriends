1ca55f4accc12db8ee6e4f98aaea90c46bc49b09
Miquel <abellagarciamiquel@gmail.com>
2023-02-02 15:47:08 +0100

    fixed js conflict

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,6 @@ const friendsSuggestionsContainer = document.querySelector(
   "#friendsSuggestionsContainer"
 );
 
-const showParagraph = document.querySelectorAll(".paragraph-show__text");
-
-showParagraph.forEach((element) => {
-  element.addEventListener("click", togglePassword);
-});
-
 document.body.addEventListener("load", getUsers());
 function getUsers() {
   fetch("./controllers/users/getAll.php")
@@ -35,18 +29,4 @@ function getUsers() {
 
 function addFriend(event) {
   console.log("Adding " + event.target.getAttribute("userId"));
-}
-
-function togglePassword(event) {
-  const clickedShow = document.querySelector("#" + event.target.id);
-  const clickedInput = document.querySelector(
-    "#" + event.target.nextElementSibling.id
-  );
-  clickedInput.type == "password"
-    ? ((clickedInput.type = "text"),
-      (clickedShow.textContent = "Hide"),
-      (clickedShow.style.color = "rgb(161, 161, 161)"))
-    : ((clickedInput.type = "password"),
-      (clickedShow.textContent = "Show"),
-      (clickedShow.style.color = "black"));
 }
--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html class="html__container--background-image" lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/css/styles.css?v=<?php echo time(); ?>">
-    <script src="./assets/js/script.js?v=<?php echo time();?>" defer></script>
+    <script src="./assets/js/script.js?v=<?php echo time(); ?>" defer></script>
+    <script src="./assets/js/register.js?v=<?php echo time(); ?>" defer></script>
     <title>beFriends - index</title>
 </head>
+
 <body>
     <header></header>
     <main class="main__container">
@@ -43,7 +46,7 @@
                 <p class="paragraph-register__text">Register</p>
             </section>
             <section class="section-third-body__container">
-                <p class="paragraph-download__text" >Download the app</p>
+                <p class="paragraph-download__text">Download the app</p>
                 <div class="div-app-icons__container">
                     <img class="icons__img" src="./assets/images/appIcon.png" alt="App store download icon">
                     <img class="icons__img" src="./assets/images/googlePlayIcon.png" alt="Google Play store download icon">
@@ -53,4 +56,5 @@
     </main>
     <footer></footer>
 </body>
+
 </html>

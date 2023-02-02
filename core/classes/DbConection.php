4ad4f245e9368fbdc079a3859b675cf10a3453b0
Miquel <abellagarciamiquel@gmail.com>
2023-02-02 10:52:28 +0100

    reordered dirs

--- core/classes/DbConection.php
+++ core/classes/DbConection.php
@@ -1,4 +1,5 @@
 <?php
+
 class DbConection
 {
     protected $db;
--- index.php
+++ index.php
@@ -1,8 +1,1 @@
-<?php
-require_once("./config/db.php");
-require_once("./core/classes/Database.php");
-// require_once("./core/classes/DbConection.php");
 
-// require_once("./models/UserModel.php");
-
-include("./views/feed.php");

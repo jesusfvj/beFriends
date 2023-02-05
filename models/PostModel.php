9f0ae7be645c7647ef29f6eeb69179cc6a61a065
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-06 00:33:35 +0100

    refactor(requires): move the requires to an index

--- models/PostModel.php
+++ models/PostModel.php
@@ -1,7 +1,5 @@
 <?php
-require_once("../core/classes/DbConection.php");
-require_once("../core/classes/Database.php");
-require_once("../config/db.php");
+require_once("../models/index.php");
 
 class PostModel extends DbConection
 {

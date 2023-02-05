83b869bf5ee3c4ebf14594835ad8a8cac214ed71
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-05 12:59:29 +0100

    update(sql): minor change

--- resources/db.sql
+++ resources/db.sql
@@ -42,20 +42,20 @@ CREATE TABLE comment(
 );
 
 INSERT INTO user(name, nickname, email, password, gender, avatar, role, created_at, updated_at)
-VALUES ('David Pizarro', 'Dave', 'david@davo.com', 'password', 'Male', 'assets/images/defaultProfileImg.png', 'admin', '2019-12-31', '2019-12-31'),
-       ('Jesusin of terror', 'Jesusito', 'jes@us.com', 'password', 'Male', 'assets/images/defaultProfileImg.png', 'admin', '2019-12-31', '2019-12-31'),
+VALUES ('David Pizarro', 'Dave', 'david@davo.com', 'password', 'Male', 'assets/images/defaultProfileImg.png', 'admin', '2017-12-31', '2017-12-31'),
+       ('Jesusin of terror', 'Jesusito', 'jes@us.com', 'password', 'Male', 'assets/images/defaultProfileImg.png', 'admin', '2018-12-31', '2018-12-31'),
        ('Miqui Bee', 'Miquibeequi', 'miquibiki@mike.com', 'password', 'Male', 'assets/images/defaultProfileImg.png', 'user', '2019-12-31', '2019-12-31'),
-       ('Wilson Mandela', 'Arbitruxo', 'wilson@robocop.com', 'password', 'Male', 'assets/images/defaultProfileImg.png', 'user', '2019-12-31', '2019-12-31');
+       ('Wilson Mandela', 'DaniWils', 'wilson@robocop.com', 'password', 'Male', 'assets/images/defaultProfileImg.png', 'user', '2020-12-31', '2020-12-31');
 
 INSERT INTO post(user_id, content, image, likes, created_at, updated_at)
-VALUES (1, 'Post lorem ipsum content', 'image.jpg', 0, '2019-12-31', '2019-12-31'),
-       (2, 'Post lorem ipsum content', 'image.jpg', 0, '2019-12-31', '2019-12-31'),
-       (3, 'Post lorem ipsum content', 'image.jpg', 0, '2019-12-31', '2019-12-31'),
-       (4, 'Post lorem ipsum content', 'image.jpg', 0, '2019-12-31', '2019-12-31');
+VALUES (1, 'Post lorem ipsum content 1', 'image.jpg', 0, '2017-12-31', '2019-12-31'),
+       (2, 'Post lorem ipsum content 2', 'image.jpg', 0, '2018-12-31', '2019-12-31'),
+       (3, 'Post lorem ipsum content 3', 'image.jpg', 0, '2019-12-31', '2019-12-31'),
+       (4, 'Post lorem ipsum content 4', 'image.jpg', 0, '2020-12-31', '2019-12-31');
 
 INSERT INTO comment(user_id, post_id, content, created_at)
-VALUES (4, 1, 'Comment ArbitruxoMachine', '2019-12-31'),
-       (3, 2, 'Comment MikibeeMachine', '2019-12-31'),
-       (2, 3, 'Comment JesusitoMachine', '2019-12-31'),
-       (1, 4, 'Comment DaveMachine', '2019-12-31');
-       (1, 3, 'Comment newComment', '2019-12-31');
+VALUES (4, 1, 'Comment DaniWilsMachine', '2019-08-05'),
+       (3, 2, 'Comment MikibeeMachine', '2019-09-12'),
+       (2, 3, 'Comment JesusitoMachine', '2019-10-16'),
+       (1, 4, 'Comment DaveMachine', '2019-11-19'),
+       (1, 3, 'Comment newComment', '2019-12-25');

3771228e0eb8cefe74b386ab390103b4112de573
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2023-02-05 22:57:25 +0100

    update(sql): add likes table

--- resources/db.sql
+++ resources/db.sql
@@ -24,7 +24,6 @@ CREATE TABLE post(
     user_id INT NOT NULL,
     content VARCHAR(50) NOT NULL,
     image VARCHAR(200) NOT NULL,
-    likes INT NOT NULL,
     created_at DATE,
     updated_at DATE,
     FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
@@ -41,17 +40,25 @@ CREATE TABLE comment(
     FOREIGN KEY (post_id) REFERENCES post(id) ON DELETE CASCADE
 );
 
+CREATE TABLE likes(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    post_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
+    FOREIGN KEY (post_id) REFERENCES post(id) ON DELETE CASCADE
+);
+
 INSERT INTO user(name, nickname, email, password, gender, avatar, role, created_at, updated_at)
 VALUES ('David Pizarro', 'Dave', 'david@davo.com', 'password', 'Male', 'assets/images/defaultProfileImg.png', 'admin', '2017-12-31', '2017-12-31'),
        ('Jesusin of terror', 'Jesusito', 'jes@us.com', 'password', 'Male', 'assets/images/defaultProfileImg.png', 'admin', '2018-12-31', '2018-12-31'),
        ('Miqui Bee', 'Miquibeequi', 'miquibiki@mike.com', 'password', 'Male', 'assets/images/defaultProfileImg.png', 'user', '2019-12-31', '2019-12-31'),
        ('Wilson Mandela', 'DaniWils', 'wilson@robocop.com', 'password', 'Male', 'assets/images/defaultProfileImg.png', 'user', '2020-12-31', '2020-12-31');
 
-INSERT INTO post(user_id, content, image, likes, created_at, updated_at)
-VALUES (1, 'Post lorem ipsum content 1', 'image.jpg', 0, '2017-12-31', '2019-12-31'),
-       (2, 'Post lorem ipsum content 2', 'image.jpg', 0, '2018-12-31', '2019-12-31'),
-       (3, 'Post lorem ipsum content 3', 'image.jpg', 0, '2019-12-31', '2019-12-31'),
-       (4, 'Post lorem ipsum content 4', 'image.jpg', 0, '2020-12-31', '2019-12-31');
+INSERT INTO post(user_id, content, image, created_at, updated_at)
+VALUES (1, 'Post lorem ipsum content 1', 'image.jpg', '2017-12-31', '2019-12-31'),
+       (2, 'Post lorem ipsum content 2', 'image.jpg', '2018-12-31', '2019-12-31'),
+       (3, 'Post lorem ipsum content 3', 'image.jpg', '2019-12-31', '2019-12-31'),
+       (4, 'Post lorem ipsum content 4', 'image.jpg', '2020-12-31', '2019-12-31');
 
 INSERT INTO comment(user_id, post_id, content, created_at)
 VALUES (4, 1, 'Comment DaniWilsMachine', '2019-08-05'),
@@ -59,3 +66,9 @@ VALUES (4, 1, 'Comment DaniWilsMachine', '2019-08-05'),
        (2, 3, 'Comment JesusitoMachine', '2019-10-16'),
        (1, 4, 'Comment DaveMachine', '2019-11-19'),
        (1, 3, 'Comment newComment', '2019-12-25');
+
+INSERT INTO likes(user_id, post_id)
+VALUES (1, 4),
+       (2, 3),
+       (3, 2),
+       (4, 1);

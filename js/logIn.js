48682317488fc4a1b52ebb84abc3f68a7d99b25a
Miquel <abellagarciamiquel@gmail.com>
2023-02-09 10:52:01 +0100

    Merge branch 'develop' into post_creation

---
+++ js/logIn.js
@@ -0,0 +1,84 @@
+const inputUserLogIn = document.querySelector("#inputUserLogIn");
+const inputPasswordLogIn = document.querySelector("#inputPasswordLogIn");
+const logInForm = document.querySelector("#formContainerLogIn");
+const registerText = document.querySelector(".paragraph-register__text");
+const showParagraph = document.querySelectorAll(".paragraph-show__text");
+const errorLoginAlert = document.querySelector(".modal-error-login__div");
+const facebookText = document.querySelector(".facebook__text");
+const facebookIcon = document.querySelector(".facebook__icon");
+const forgotText = document.querySelector(".paragraph-forgot__text");
+
+const iconsImg = document.querySelectorAll(".icons__img");
+const comingSoonAlert = document.querySelector(".modal-coming-soon__div");
+
+facebookText.addEventListener("click", showUnderConstModal);
+facebookIcon.addEventListener("click", showUnderConstModal);
+forgotText.addEventListener("click", showUnderConstModal);
+
+iconsImg.forEach((element) => {
+  element.addEventListener("click", showUnderConstModal);
+});
+
+registerText.addEventListener("click", sendToRegister);
+
+logInForm.addEventListener("submit", sendLogInData);
+
+showParagraph.forEach((element) => {
+  element.addEventListener("click", togglePassword);
+});
+
+function sendToRegister() {
+  window.location = "./register.php";
+}
+
+function sendLogInData(event) {
+  event.preventDefault();
+  const formData = new FormData();
+  inputUserLogIn.value.includes("@")
+    ? (formData.append("email", inputUserLogIn.value),
+      formData.append("username", ""))
+    : (formData.append("username", inputUserLogIn.value),
+      formData.append("email", ""));
+  formData.append("password", inputPasswordLogIn.value);
+  fetch("./controllers/users.php?controller=login", {
+    method: "POST",
+    body: formData,
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data[0] === true) {
+        localStorage.setItem("userId", JSON.stringify(data.userId));
+        window.location = "./feed.php";
+      } else {
+        modalVerificationsLogin();
+      }
+    });
+}
+
+function showUnderConstModal(){
+  comingSoonAlert.style.display = "block";
+    setTimeout(() => {
+      comingSoonAlert.style.display = "none";
+    }, 2000);
+}
+
+function modalVerificationsLogin(){
+  errorLoginAlert.style.display = "block";
+    setTimeout(() => {
+      errorLoginAlert.style.display = "none";
+    }, 3000);
+}
+
+function togglePassword(event) {
+  const clickedShow = document.querySelector("#" + event.target.id);
+  const clickedInput = document.querySelector(
+    "#" + event.target.nextElementSibling.id
+  );
+  clickedInput.type == "password"
+    ? ((clickedInput.type = "text"),
+      (clickedShow.textContent = "Hide"),
+      (clickedShow.style.color = "rgb(161, 161, 161)"))
+    : ((clickedInput.type = "password"),
+      (clickedShow.textContent = "Show"),
+      (clickedShow.style.color = "black"));
+}
---
+++ js/register.js
@@ -0,0 +1,102 @@
+const showParagraph = document.querySelectorAll(".paragraph-show__text");
+const passwordAlert = document.querySelector(".password-verification__div--display");
+const sucessAlert = document.querySelector(".modal-success-register__div");
+const errorAlert = document.querySelector(".modal-error-register__div");
+const logInText = document.querySelector(".paragraph-log-in__text");
+const registerForm = document.querySelector("#formContainerRegister");
+const registerErrorMessage = document.getElementById('registerError');
+
+
+const iconsImg = document.querySelectorAll(".icons__img");
+const comingSoonAlert = document.querySelector(".modal-coming-soon__div");
+
+
+iconsImg.forEach((element) => {
+  element.addEventListener("click", showUnderConstModal);
+});
+
+showParagraph.forEach((element) => {
+  element.addEventListener("click", togglePassword);
+});
+
+registerForm.addEventListener("submit", sendRegisterData);
+
+logInText.addEventListener("click", sendToLogIn);
+
+function showUnderConstModal(){
+  comingSoonAlert.style.display = "block";
+    setTimeout(() => {
+      comingSoonAlert.style.display = "none";
+    }, 2000);
+}
+
+function sendToLogIn(){
+  window.location="./index.php";
+}
+
+function sendRegisterData(event) {
+  event.preventDefault();
+  if (inputPasswordRepeat.value === inputPasswordRegister.value) {
+    const formData = new FormData();
+    formData.append("fullname", inputNameRegister.value);
+    formData.append("username", inputUserRegister.value);
+    formData.append("email", inputEmailRegister.value);
+    formData.append("password", inputPasswordRegister.value);
+    formData.append("gender", inputGenderRegister.value);
+    fetch("./controllers/users.php?controller=register", {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data[0] && data[0] == 'password-strength') {
+          registerErrorMessage.textContent = data[1];
+        } else if (data[0] && data[0] == 'user-or-email-not-valid') {
+          registerErrorMessage.textContent = data[1];
+        } else if (data[0] && data[0] == 'username-length') {
+          registerErrorMessage.textContent = data[1];
+        }
+        modalVerifications(data);
+      });
+  } else {
+    showPasswordVerification();
+  }
+}
+
+function modalVerifications(data){
+  if (data[0] === true) {
+    sucessAlert.style.display = "block";
+    setTimeout(() => {
+      sucessAlert.style.display = "none";
+    }, 2000);
+    setTimeout(() => {
+      window.location.href = "./index.php";
+    }, 2200);
+  } else{
+    errorAlert.style.display = "block";
+    setTimeout(() => {
+      errorAlert.style.display = "none";
+    }, 4000);
+  }
+}
+
+function showPasswordVerification() {
+  passwordAlert.style.display = "block";
+  window.addEventListener("click", () => {
+    passwordAlert.style.display = "none";
+  })
+}
+
+function togglePassword(event) {
+  const clickedShow = document.querySelector("#" + event.target.id);
+  const clickedInput = document.querySelector(
+    "#" + event.target.nextElementSibling.id
+  );
+  clickedInput.type == "password"
+    ? ((clickedInput.type = "text"),
+      (clickedShow.textContent = "Hide"),
+      (clickedShow.style.color = "rgb(161, 161, 161)"))
+    : ((clickedInput.type = "password"),
+      (clickedShow.textContent = "Show"),
+      (clickedShow.style.color = "black"));
+}
